9f31fda4c6087ebe7d76fb235c8f50c78af1e0d4
Fatih Etlik <fatihe307@gmail.com>
2024-08-26 08:13:05 +0300

    changes

--- QRGenerator/src/middlewares/helmet/middleware.js
+++ QRGenerator/src/middlewares/helmet/middleware.js
@@ -7,8 +7,15 @@ middleware.use(helmet({
     useDefaults: true,
     directives: {
       "default-src": ["'self'"],
-      "script-src": ["'self'", "trusted-cdn.com"], // Only allow scripts from self and trusted CDNs
-      "img-src": ["'self'", "data:", "trusted-cdn.com"], // Allow images from self, data URLs, and trusted CDNs
+      "script-src": ["'self'", "trusted-cdn.com"], // Allow scripts from self and trusted CDNs
+      "img-src": [
+        "'self'", 
+        "data:", 
+        "trusted-cdn.com", 
+        process.env.CLIENT_ADMIN,  // Allow images from the admin client domain
+        process.env.CLIENT_USER,   // Allow images from the user client domain
+        `${process.env.VITE_ENV_SERVER_URL}/api/user/getFile`  // Allow images from your API endpoint
+      ],
       "style-src": ["'self'", "'unsafe-inline'", "trusted-cdn.com"], // Allow inline styles and styles from trusted CDNs
       "connect-src": ["'self'", process.env.CLIENT_ADMIN, process.env.CLIENT_USER], // Allow connections only to specific origins
     },
