03e44889d1bc567e08aa4e4f1b77a936e843d685
Fatih Etlik <fatihe307@gmail.com>
2024-08-30 12:36:13 +0300

    big changes

--- QRGenerator/src/middlewares/cors/middleware.js
+++ QRGenerator/src/middlewares/cors/middleware.js
@@ -6,10 +6,7 @@ const middleware = express();
 const corsOptions = {
   origin: [
     `${process.env.CLIENT_ADMIN}`, 
-    `${process.env.CLIENT_USER}`,
-    `${process.env.CLIENT_USER}/adminPanel`,
-    `${process.env.CLIENT_USER}/adminPanel/login`,
-    `${process.env.CLIENT_USER}/adminPanel/adminOperations`
+    `${process.env.CLIENT_USER}`
   ], // Only allow these origins
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
